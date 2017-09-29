f6d71c62f18b447e36e5d314d19d2e9f64c135ae
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-29 15:46:53 -0700

    feat(getdictionarypath): implements dictionary path lookup

---
+++ src/getDictionaryPath.ts
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import { getLanguageRegionCode } from './getLanguageRegionCode';
+import { CODE_LANG, CODE_LANG_REGION } from './manifest';
+
+/**
+ * Returns path to dictionary files for given langauge.
+ * This doesn't check existense of dictionary if it's already installed locally or not.
+ *
+ * @param {CODE_LANG_REGION | CODE_LANG} code
+ */
+export const getDictionaryPath: (
+  dictionaryDirectory: string,
+  code: CODE_LANG_REGION | CODE_LANG
+) => Readonly<{ installDirectory: string; dic: string; aff: string }> = (
+  dictionaryDirectory: string,
+  code: CODE_LANG_REGION | CODE_LANG
+) => {
+  const langRegionCode = getLanguageRegionCode(code);
+  const installDirectory = path.join(dictionaryDirectory, langRegionCode);
+
+  return {
+    installDirectory,
+    dic: path.join(installDirectory, `${langRegionCode}.dic`),
+    aff: path.join(installDirectory, `${langRegionCode}.aff`)
+  };
+};
