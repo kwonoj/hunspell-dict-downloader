b1e8ae5423f6e3d4f44bd06430e82749cc03c8ce
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-29 15:48:47 -0700

    feat(index): exports modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export {};
+export { CODE_LANG_REGION, CODE_LANG } from './manifest';
+export { HunspellDictDownloader } from './HunspellDictDownloader';
+export { createDownloader } from './createDownloader';
