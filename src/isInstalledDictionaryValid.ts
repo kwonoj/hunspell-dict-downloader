c76a610fab8cb297ab7e071284f019eed447dbb6
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-29 23:55:04 -0700

    refactor(isinstalleddictionaryvalid): add logs

--- src/isInstalledDictionaryValid.ts
+++ src/isInstalledDictionaryValid.ts
@@ -19,15 +19,21 @@ export const isInstalledDictionaryValid: (
 ) => Promise<boolean> = async (dictionaryDirectory: string, code: CODE_LANG_REGION | CODE_LANG) => {
   const langRegionCode = getLanguageRegionCode(code);
   if (!await isLocalDictionaryExists(dictionaryDirectory, langRegionCode)) {
+    log.info(`isInstalledDictionaryValid: '${langRegionCode}' is not installed`);
     return false;
   }
 
   const { dicMD5, affMD5 } = manifest[langRegionCode] as DictionaryManifest;
   const { dic, aff } = getDictionaryPath(dictionaryDirectory, langRegionCode);
 
+  log.silly(`isInstalledDictionaryValid: checksum`, { dicMD5, affMD5 });
+  log.silly(`isInstalledDictionaryValid: checking`, { dic, aff });
+
   try {
     const dicHash = await md5(dic);
     const affHash = await md5(aff);
+
+    log.silly(`isInstalledDictionaryValid: read`, { dicHash, affHash });
 
     const isValid = dicHash === dicMD5 && affHash === affMD5;
     log.info(`isInstalledDictionaryValid: dictionary for ${langRegionCode} validated`, isValid);
