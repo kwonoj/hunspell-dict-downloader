995140ac3e23af988a1699b0efd3ce7ae9fa1f73
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-29 23:11:00 -0700

    refactor(getdictionarypath): simplify function

--- src/getDictionaryPath.ts
+++ src/getDictionaryPath.ts
@@ -1,27 +1,21 @@
 import * as path from 'path';
-import { getLanguageRegionCode } from './getLanguageRegionCode';
-import { CODE_LANG, CODE_LANG_REGION } from './manifest';
+import { CODE_LANG_REGION } from './manifest';
 
 /**
  * @internal
  * Returns path to dictionary files for given langauge.
  * This doesn't check existense of dictionary if it's already installed locally or not.
  *
- * @param {CODE_LANG_REGION | CODE_LANG} code
+ * @param {CODE_LANG_REGION} code
  */
 export const getDictionaryPath: (
   dictionaryDirectory: string,
-  code: CODE_LANG_REGION | CODE_LANG
+  code: CODE_LANG_REGION
 ) => Readonly<{ installDirectory: string; dic: string; aff: string }> = (
   dictionaryDirectory: string,
-  code: CODE_LANG_REGION | CODE_LANG
-) => {
-  const langRegionCode = getLanguageRegionCode(code);
-  const installDirectory = path.join(dictionaryDirectory, langRegionCode);
-
-  return {
-    installDirectory,
-    dic: path.join(installDirectory, `${langRegionCode}.dic`),
-    aff: path.join(installDirectory, `${langRegionCode}.aff`)
-  };
-};
+  code: CODE_LANG_REGION
+) => ({
+  installDirectory: path.join(dictionaryDirectory, code),
+  dic: path.join(dictionaryDirectory, code, `${code}.dic`),
+  aff: path.join(dictionaryDirectory, code, `${code}.aff`)
+});
