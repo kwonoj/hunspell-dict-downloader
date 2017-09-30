0b1d18b4e9aca5b0158c3d82632d89f62dc05683
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-30 13:07:56 -0700

    feat(index): export logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { CODE_LANG_REGION, CODE_LANG } from './manifest';
 export { HunspellDictDownloader } from './HunspellDictDownloader';
 export { createDownloader } from './createDownloader';
+export { enableLogger } from './util/logger';
